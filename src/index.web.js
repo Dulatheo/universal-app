c2f166867ff070d82683bd910dec1251c58336d8
Dulat Yessilbayev <d.yessilbayev@gmail.com>
2025-04-07 18:44:41 +0500

    Added creating items and the list of items

--- src/index.web.js
+++ src/index.web.js
@@ -1,5 +1,5 @@
 import {AppRegistry} from 'react-native';
-import App from './App'; // Fixed relative path
+import App from '../App'; // Fixed relative path
 import {name} from './app.json'; // Fixed relative path
 
 AppRegistry.registerComponent(name, () => App);
